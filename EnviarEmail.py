1ca9811c776f97aba3f875ad75c35c128719fdc2
alyssonmilet <alyssonmilet@yahoo.com.br>
2021-05-31 06:41:31 -0300

    readme

--- EnviarEmail.py
+++ EnviarEmail.py
@@ -1,45 +1,42 @@
+#Enviando e-mail com Python
+# Alysson Milet
+# github.com/alyssonmilet
+
+#Bibliotecas
 import smtplib
-# Biblioteca SMTP para envio de e-mails
-
 from email.mime.multipart import MIMEMultipart
-#
 from email.mime.text import MIMEText
-#
 
 
-#Programa
-# SMTP - Protocolo de envio de e-mail
-#dados iniciais
-destinatario = str(input("Qual endereço de envio:"))
-mensagem = str(input("Digite a mensagem que deseja enviar:"))
+#Aplicativo
 
+try: 
+    destinatario = str(input("Qual endereço de envio:"))
+    mensagem = str(input("Digite a mensagem que deseja enviar:"))
 
-# 1 - Iniciar o servidor SMTP
-host = "smtp.gmail.com"
-porta = "587"
-login = "emaildeexemplo.python@gmail.com"
-senha = "123@#$345"
+    host = "smtp.gmail.com"
+    porta = "587"
+    login = "emaildeexemplo.python@gmail.com"
+    senha = "123@#$345
+    
+    servidor = smtplib.SMTP(host,porta)
 
-servidor = smtplib.SMTP(host,porta)
+    servidor.ehlo()
+    servidor.starttls()
 
-servidor.ehlo()
-servidor.starttls()
+    servidor.login(login,senha)
 
-servidor.login(login,senha)
+    email_msg = MIMEMultipart()
+    email_msg['From'] = login
+    email_msg['To'] = destinatario
+    email_msg['Subject'] = "Meu e-mail enviado"
+    email_msg.attach(MIMEText(mensagem,'plain'))
+    # email_msg.attach(MIMEText(mensagem,'html'))
 
-# 2 - Contruir a mensagem do tipo MIME
+    servidor.sendmail(email_msg['From'],email_msg['To'], email_msg.as_string())
+        
+    servidor.quit()
+    print(f'E-mail enviado com sucesso para {destinatario}')
 
-
-email_msg = MIMEMultipart()
-email_msg['From'] = login
-email_msg['To'] = destinatario
-email_msg['Subject'] = "Meu e-mail enviado"
-email_msg.attach(MIMEText(mensagem,'plain'))
-# email_msg.attach(MIMEText(mensagem,'html'))
-
-
-
-# 3 - Enviar o e-mail
-servidor.sendmail(email_msg['From'],email_msg['To'], email_msg.as_string())
-servidor.quit()
-print(f'E-mail enviado com sucesso para {destinatario}')
+except smtplib.SMTPException:
+    print("Erro ao enviar e-mail")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Enviar_Email_com_Python
  Repositório que explica como enviar e-mail com Python
+
+ Documentação
+ https://docs.python.org/pt-br/3.7/library/email.html
+
+ 
